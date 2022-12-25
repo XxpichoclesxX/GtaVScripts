293c2256a429091d7159eb7f001fa6e412f610f2
XxpichoclesxX <81063926+XxpichoclesxX@users.noreply.github.com>
2022-12-24 19:22:15 -0600

    Update snowmen.lua

--- Stand2/snowmen.lua
+++ Stand2/snowmen.lua
@@ -1,36 +1,64 @@
+util.require_natives("natives-1663599433")
+
 local snowmen = {
-    { -955.5352, -782.4186, 15.925326 },
+    { -959.2467, -780.8681, 17.83611 },
     { -819.9067, 165.35524, 71.26295 },
-    { -247.92003, -1561.2175, 32.22973 },
-    { 901.3996, -288.05026, 65.643715 },
-    { 1275.6772, -642.3789, 68.208786 },
+    { -251.52495, -1561.682, 31.939138 },
+    { 901.7144, -286.2657, 65.673515 },
+    { 1270.7291, -645.9963, 68.00145 },
     { 1993.1008, 3829.6396, 32.167336 },
     { -375.47916, 6230.158, 31.490055 },
-    { -1414.4647, 5094.9272, 60.674263 },
-    { -1946.0231, 594.93756, 119.79223 },
+    { -1414.2078, 5101.0576, 60.303204 },
+    { -1938.8783, 589.47314, 119.70296 },
     { -1100.889, -1401.0138, 5.215467 },
-    { -780.418, 877.2859, 203.18001 },
-    { -455.12027, 1130.5625, 325.89175 },
-    { 221.46722, -107.01075, 69.763985 },
-    { 1560.3787, 6447.053, 23.9866 },
-    { 3301.7886, 5149.8853, 18.34274 },
-    { 1703.0568, 4671.928, 43.15301 },
-    { 224.9161, 3107.3345, 42.18394 },
-    { 2360.0037, 2522.7166, 46.66731 },
-    { 1510.8555, 1725.0353, 109.89355 },
-    { -48.96374, 1964.4028, 189.87181 },
+    { -778.11993, 878.8288, 203.31508 },
+    { -455.0701, 1129.6311, 325.88016 },
+    { 219.26233, -104.535065, 69.743095 },
+    { 1559.2108, 6449.2827, 23.849245},
+    { 3313.9011, 5164.719, 18.415358 },
+    { 1708.7146, 4679.3745, 42.977077 },
+    { 234.04605, 3103.639, 42.46318},
+    { 2359.1873, 2524.3604, 46.585716 },
+    { 1514.7108, 1721.1183, 110.25117 },
+    { -48.133442, 1963.3002, 189.96031 },
     { -1516.2083, 2140.2168, 56.11924 },
-    { -2826.2356, 1424.6211, 100.706116 },
-    { -2957.531, 723.6389, 28.792404 },
+    { -2830.0112, 1421.0872, 100.90339 },
+    { -2974.723, 714.6455, 28.35867 },
     { 1341.4302, -1583.5093, 54.135414 },
-    { 178.00232, -895.9712, 30.69326 }
+    { 180.52608, -903.90607, 30.693544 }
 }
 
-local snow_loca = menu.list(menu.my_root(), "Snowmen", {}, "")
+-- Credits to blackn't to add explode snowmen
+
+local snow_loca = menu.my_root()
+local explode_snowmen = true
+local god_path = menu.ref_by_path("Self>Immortality")
+
+snow_loca:divider("Snowmen Teleports")
+
+auto_exp_toggle= true
+auto_exp_toggle = snow_loca:toggle("Auto Explode Snowmen on Tp", {}, "", function(toggle)
+    if toggle then
+        explode_snowmen = true
+        util.toast("Enabled Auto Explode Snowmen")
+    else
+        explode_snowmen = false
+        util.toast("Disabled Auto Explode Snowmen")
+    end
+end, true)
+
 for idx, coords in snowmen do
-    snow_loca:action("Snowmen " .. idx, {}, "Teleport to XMass", function()
+    snow_loca:action("Snowman #" .. idx, {}, "Teleport to Snowman #".. idx, function()
         util.teleport_2d(coords[1], coords[2])
+        util.yield(100)
+        if explode_snowmen == true then
+            menu.trigger_command(god_path, "on")
+            util.yield(750)
+            for i = 1, 10 do
+            FIRE.ADD_OWNED_EXPLOSION(PLAYER.PLAYER_PED_ID(),coords[1], coords[2], coords[3] + 1, EXP_TAG_STICKYBOMB, 100.0, true, true, 0.0)
+            util.yield(100)
+            end
+        end
     end)
 end
-
 util.keep_running()
