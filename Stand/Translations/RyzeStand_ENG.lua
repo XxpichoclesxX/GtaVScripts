fa684148db49b9bdd633b82cdc4a3dec0bb23fdb
XxpichoclesxX <81063926+XxpichoclesxX@users.noreply.github.com>
2022-12-27 14:48:59 -0600

    Update RyzeStand_ENG.lua

--- Stand/Translations/RyzeStand_ENG.lua
+++ Stand/Translations/RyzeStand_ENG.lua
@@ -11,7 +11,7 @@ util.require_natives(1663599433)
 util.toast("Welcome " .. SOCIALCLUB.SC_ACCOUNT_INFO_GET_NICKNAME() .. " to the script!!")
 util.toast("Loading, wait... (1-2s)")
 local response = false
-local localVer = 3.876
+local localVer = 3.877
 async_http.init("raw.githubusercontent.com", "/XxpichoclesxX/GtaVScripts/Ryze-Scripts/Stand/RyzeScriptVersion.lua", function(output)
     currentVer = tonumber(output)
     response = true
